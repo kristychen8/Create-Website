45edf25d6eea5412ae041d56dc9dbdd38d5fb88c
Tina Lee <l.tn3314@gmail.com>
2016-10-01 16:34:45 -0400

    changing background when hovering in sidebar

--- website.js
+++ website.js
@@ -1,4 +1,4 @@
-
+// hide sidebar at beginning
 $('#sidebarContents').css('width','0');
 
 // opening sidebar
@@ -9,4 +9,23 @@ $('#lines').click(function() {
 // closing sidebar
 $('#x').click(function() {
     $('#sidebarContents').css('width', '0');
+});
+
+// change background when hovering over sidebar elements
+$('#sidebarContents li').hover(function() {
+    $(this).css('background-color', '#008B8B');
+}, function() {
+    $(this).css('background-color', 'rgb(50,50,50)');
+});
+
+// change background when hovering over top of sidebar
+$('#top').hover(function() {
+    $(this).css('background-color', 'rgb(100,100,100)');
+}, function() {
+    $(this).css('background-color', 'rgb(50,50,50)');
+});
+
+// set page to top (clicking top of sidebar)
+$('#top').click(function() {
+    $('html body').animate({scrollTop: 0}, 'fast');
 });
